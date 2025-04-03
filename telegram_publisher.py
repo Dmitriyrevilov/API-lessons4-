1ef20bd465f79362cbb91ee4333aca25e866d7d1
Dmitriyrevilov <dimrevilov@gmail.com>
2025-04-03 19:50:58 +0500

    Update telegram_publisher.py

--- telegram_publisher.py
+++ telegram_publisher.py
@@ -3,15 +3,10 @@ from dotenv import load_dotenv
 import os
 
 
-
-from download_apod import download_apod_images 
+from download_apod import download_apod_images
 from download_epic import download_epic_images
 from fetch_spacex_images import fetch_spacex_images
 from coolprogram import download_image
-
-
-
-
 
 
 load_dotenv()
@@ -25,7 +20,6 @@ chat_id = bot.get_updates()[-1].message.chat_id
 
 bot.send_message(chat_id=chat_id, text="Привет")
 
-try:
-    with open("image.jpg", "rb") as photo:
-        bot.send_photo(chat_id=chat_id, photo=photo, caption="Фото из космоса")
-    print("Фотография успешно отправлена!")
+with open("image.jpg", "rb") as photo:
+    bot.send_photo(chat_id=chat_id, photo=photo, caption="Фото из космоса")
+print("Фотография успешно отправлена!")
